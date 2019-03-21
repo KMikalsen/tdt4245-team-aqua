99b793d2db0578ae36ab8017faa086b8fa947854
Christian Bernard Bouwhuis Røkke <christian.b.rokke@gmail.com>
2019-03-21 10:13:06 +0100

    Added feedback and translated to English

--- cards-frontend/src/data/cards.js
+++ cards-frontend/src/data/cards.js
@@ -2,7 +2,7 @@ const scenarios = {
     's1':{
         id:"S1",
         title:"Picture sharing",
-        description:"Noen har delt et bilde av deg du ikke ønsker skal deles"
+        description:"Someone shared a picture fo you that you are not comfortable with being shared."
     },
 }
 
@@ -11,37 +11,46 @@ const cards = [
         id:"C1",
         scenario:"1",
         title:"Admins",
-        description:"Kontakt administrator"
+        description:"Contact the administrator",
+        feedback:"This is a good idea. If a user is unwilling to remove a picture of you that " +
+            "they do not have permission to share, administrator have an obligation to help. Should it be your first move?"
     },
     {
         id:"C2",
         scenario:"1",
         title: "Comment",
-        description:"Kommenter på bilde og be de om å fjerne det"
+        description:"Comment on the picture asking for it to be removed.",
+        feedback:"Most people can be assumed not to be malicious, and will often comply if asked. What if they don't though?"
     },
     {
         id:"C3",
         scenario:"1",
         title: "Hitman",
-        description:"Lei en hitman"
+        description:"Buy a hitman!",
+        feedback:"In the current market, it may be difficult to acquire a hitman. They are quite expensive, you know?"
     },
     {
         id:"C4",
         scenario:"1",
-        title: "Share",
-        description:"Del et bilde av de som blackmail"
+        title: "Retribution",
+        description:"Share a picture of them that is equally embarrassing to them.",
+        feedback:"Do you really want to stoop to their level?"
     },
     {
         id:"C5",
         scenario:"1",
         title: "Contact",
-        description:"Kontakte de privat og forklar hvorfor du ikke synes det er greit"
+        description:"Contact them privately and explain to them why it's not ok.",
+        feedback:"Contacting someone privately may make them more likely to help. It's not fun to be called out publicly" +
+            " for something you maybe didn't realise was wrong."
     },
     {
         id:"C6",
         scenario:"1",
-        title: "Tell",
-        description:"Fortell det til alle du kjenner slik at de kan hjelp deg"
+        title: "Crowdsource",
+        description:"Tell everyone you know so that they can help you get it removed.",
+        feedback:"Receiving a torrent of request from many people to remove something will definitely have an effect, " +
+            "but will you be responsible for what everyone you know says to this person in anger?"
     },
 ]
 
