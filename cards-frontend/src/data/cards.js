1613baadcfe06550677f805531cb885f0589fef4
Christian Bernard Bouwhuis Røkke <christian.b.rokke@gmail.com>
2019-03-21 10:45:40 +0100

    Added scenario 2 with cards

--- cards-frontend/src/data/cards.js
+++ cards-frontend/src/data/cards.js
@@ -2,8 +2,15 @@ const scenarios = {
     's1':{
         id:"S1",
         title:"Picture sharing",
-        description:"Someone shared a picture fo you that you are not comfortable with being shared."
+        description:"Someone shared a picture fo you that you are not comfortable with being shared.",
+        cardIDs:["C1", "C2", "C3", "C4", "C4", "C5", "C6"]
     },
+    's2':{
+        id:"S2",
+        title:"Friend request",
+        description:"You received a friend request from someone you don't know.",
+        cardIDs:[]
+    }
 }
 
 const cards = [
@@ -51,6 +58,41 @@ const cards = [
         description:"Tell everyone you know so that they can help you get it removed.",
         feedback:"Receiving a torrent of request from many people to remove something will definitely have an effect, " +
             "but will you be responsible for what everyone you know says to this person in anger?"
+    },
+    {
+        id:"C7",
+        scenario:"2",
+        title:"Accept it",
+        description:"The more the merrier.",
+        feedback:"Maybe use a bit more scrutiny? They could be out to trick you."
+    },
+    {
+        id:"C8",
+        scenario:"2",
+        title:"Ask them",
+        description:"Ask them who they are. You may know the from somewhere but forgot.",
+        feedback:"This is a good idea. I can rarely hurt to ask!"
+    },
+    {
+        id:"C9",
+        scenario:"2",
+        title:"Decline it",
+        description:"New phone, who dis? Maybe asking wouldn't have been a bad idea though?",
+        feedback:""
+    },
+    {
+        id:"C10",
+        scenario:"2",
+        title:"Investigate",
+        description:"Search through their photos and proflie in an attempt to figure out who this is.",
+        feedback:"This could definitely help you figure out who they are. Does it feel right?"
+    },
+    {
+        id:"C11",
+        scenario:"2",
+        title:"Leave it",
+        description:"Leave them hanging, id you don't remember them, it's not worth responding.",
+        feedback:"This is a bit rude. Certainly there are other ways to sort this out?"
     },
 ]
 
